558f931eb04f4b101bcba9b6a36dc6bbe1c2517f
Kevin <nivekyang@yahoo.com>
2019-03-13 16:19:05 -0600

    Basic rendering of issues

--- AngularIssues/src/app/app.module.ts
+++ AngularIssues/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // TODO: Import only what I need
 
 import { AppComponent } from './app.component';
 import { IssueService } from './issue-service/issue.service';
@@ -12,6 +13,7 @@ import { IssuesListComponent } from './issues-list/issues-list.component';
     IssuesListComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     HttpClientModule,
   ],
